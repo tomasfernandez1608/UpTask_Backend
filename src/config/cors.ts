0be5268695848a5b9e243cd99bba0829e6e6763c
tomasfernandez1608 <tomasfernandez1608@hotmail.com>
2026-03-25 12:36:21 -0300

    debug cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,16 +1,22 @@
-import { CorsOptions } from 'cors'
+import { CorsOptions } from 'cors'
 
 export const corsConfig: CorsOptions = {
-    origin: function(origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL]
+    origin: function (origin, callback) {
 
-        if(process.argv[2] === '--api') {
-            whitelist.push(undefined)
+        const whitelist = [
+            process.env.FRONTEND_URL,
+            "http://localhost:5173"
+        ]
+
+        // Permitir requests sin origin (Postman, mobile apps, etc)
+        if (!origin) {
+            return callback(null, true)
         }
 
-        if(whitelist.includes(origin)) {
+        if (whitelist.includes(origin)) {
             callback(null, true)
         } else {
+            console.log("❌ CORS bloqueado:", origin)
             callback(new Error('Error de CORS'))
         }
     }
